c88245592c33c04763640c459565aeed2f34a95e
dudulive <dudurodriguesbraz@hotmail.com>
2017-05-02 23:59:17 -0300

    Implementação do Menu Toolbar

--- curso-ionic-2/src/app/app.component.ts
+++ curso-ionic-2/src/app/app.component.ts
@@ -1,22 +1,32 @@
+import { HomePage } from './../pages/home/home';
 import { Component } from '@angular/core';
 import { Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
-
-import { HomePage } from '../pages/home/home';
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
+
+  pages: Array<{ component: any, title: string , icon: any}>;
   rootPage:any = HomePage;
 
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
+
+    this.pages = [
+      {component: HomePage, title: 'Home', icon: 'home'},
+    ];
+
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       splashScreen.hide();
     });
+  }
+
+  openPage(page: any) : void {
+    this.rootPage = page.component;
   }
 }
 
