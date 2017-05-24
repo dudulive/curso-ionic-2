ca1e8c30a07edda1004f441b2cbe10566e6f4322
dudulive <edurodriguesbraz@hotmail.com>
2017-05-23 22:06:32 -0300

    Adicionado o Pipe LowerCase

--- curso-ionic-2/src/pages/menu-teste/menu-teste.module.ts
+++ curso-ionic-2/src/pages/menu-teste/menu-teste.module.ts
@@ -1,10 +1,11 @@
+import { Lowercase } from './../../pipes/lowercase';
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
 import { MenuTeste } from './menu-teste';
 
 @NgModule({
   declarations: [
-    MenuTeste,
+    MenuTeste, Lowercase
   ],
   imports: [
     IonicPageModule.forChild(MenuTeste),
